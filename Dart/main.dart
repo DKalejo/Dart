4ee53a979ddaec754d863a9cbdb385d50a989d23
Alejandro <alejandrogranadaperez200@gmail.com>
2025-08-06 09:27:52 -0500

    Cambios con el menu

--- Dart/main.dart
+++ Dart/main.dart
@@ -2,73 +2,85 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {
-  print("Menu: \n 1). MRU \n 2). Promedio de notas \n 3). Puntuacion de equipo de futbol \n 4). Planilla de empleado \n 5). Calculo de hipotenusa \n 6). Conversion de temperatura");
-  int? option = int.tryParse(stdin.readLineSync()!);
+  while (true) {
+    print(
+      "Menu: \n 1). MRU \n 2). Promedio de notas \n 3). Puntuacion de equipo de futbol \n 4). Planilla de empleado \n 5). Calculo de hipotenusa \n 6). Conversion de temperatura \n 7). Salir",
+    );
+    int? option = int.tryParse(stdin.readLineSync()!);
 
-  switch (option) {
-    case 1:
-      print("Ingrese la velocidad del vehiculo en Mt/s");
-      double? v = double.tryParse(stdin.readLineSync()!);
-      print("Ingrese el tiempo estimado en Sg");
-      double? t = double.tryParse(stdin.readLineSync()!);
-      print("Distancia recorrida: ${MRU(v, t)?.toStringAsFixed(2)} Mt");
-      break;
-    case 2:
-      List<double> listNotes = [];
-      int i = 1;
-      while (true) {
-        print("Ingrese su nota #${i} (o presione Enter para terminar):");
-        String? input = stdin.readLineSync();
+    switch (option) {
+      case 1:
+        print("Ingrese la velocidad del vehiculo en Mt/s");
+        double? v = double.tryParse(stdin.readLineSync()!);
+        print("Ingrese el tiempo estimado en Sg");
+        double? t = double.tryParse(stdin.readLineSync()!);
+        print("Distancia recorrida: ${MRU(v, t)?.toStringAsFixed(2)} Mt");
+        continue;
+      case 2:
+        List<double> listNotes = [];
+        int i = 1;
+        while (true) {
+          print("Ingrese su nota #${i} (o presione Enter para terminar):");
+          String? input = stdin.readLineSync();
 
-        if (input == null || input.isEmpty) {
-          break;
+          if (input == null || input.isEmpty) {
+            break;
+          }
+
+          double? note = double.tryParse(input);
+          if (note != null) {
+            listNotes.add(note);
+            i++;
+          } else {
+            print("Entrada inválida. Por favor, ingrese un número.");
+          }
         }
+        print(
+          "El promedio de las notas es: ${PROM(listNotes).toStringAsFixed(2)}",
+        );
+        continue;
+      case 3:
+        print("Ingrese la cantidad de partidos ganados:");
+        int? partidosGanados = int.tryParse(stdin.readLineSync()!);
+        print("Ingrese la cantidad de partidos empatados:");
+        int? partidosEmpatados = int.tryParse(stdin.readLineSync()!);
+        print("Ingrese la cantidad de partidos perdidos:");
+        int? partidosPerdidos = int.tryParse(stdin.readLineSync()!);
 
-        double? note = double.tryParse(input);
-        if (note != null) {
-          listNotes.add(note);
-          i++;
-        } else {
-          print("Entrada inválida. Por favor, ingrese un número.");
-        }
-      }
-      print("El promedio de las notas es: ${PROM(listNotes).toStringAsFixed(2)}");
-      break;
-    case 3:
-      print("Ingrese la cantidad de partidos ganados:");
-      int? partidosGanados = int.tryParse(stdin.readLineSync()!);
-      print("Ingrese la cantidad de partidos empatados:");
-      int? partidosEmpatados = int.tryParse(stdin.readLineSync()!);
-      print("Ingrese la cantidad de partidos perdidos:");
-      int? partidosPerdidos = int.tryParse(stdin.readLineSync()!);
+        puntuacionFutbol(partidosGanados, partidosEmpatados, partidosPerdidos);
+        continue;
+      case 4:
+        print("Ingrese el nombre del empleado:");
+        String? nombreEmpleado = stdin.readLineSync();
+        print("Ingrese la cantidad de horas laboradas en el mes:");
+        double? horasLaboradas = double.tryParse(stdin.readLineSync()!);
+        print("Ingrese la tarifa por hora:");
+        double? tarifaHora = double.tryParse(stdin.readLineSync()!);
 
-      puntuacionFutbol(partidosGanados, partidosEmpatados, partidosPerdidos);
-      break;
-    case 4:
-      print("Ingrese el nombre del empleado:");
-      String? nombreEmpleado = stdin.readLineSync();
-      print("Ingrese la cantidad de horas laboradas en el mes:");
-      double? horasLaboradas = double.tryParse(stdin.readLineSync()!);
-      print("Ingrese la tarifa por hora:");
-      double? tarifaHora = double.tryParse(stdin.readLineSync()!);
+        planillaEmpleado(nombreEmpleado, horasLaboradas, tarifaHora);
+        continue;
+      case 5:
+        print("Ingrese la longitud del cateto a:");
+        double? catetoA = double.tryParse(stdin.readLineSync()!);
+        print("Ingrese la longitud del cateto b:");
+        double? catetoB = double.tryParse(stdin.readLineSync()!);
 
-      planillaEmpleado(nombreEmpleado, horasLaboradas, tarifaHora);
-      break;
-    case 5:
-      print("Ingrese la longitud del cateto a:");
-      double? catetoA = double.tryParse(stdin.readLineSync()!);
-      print("Ingrese la longitud del cateto b:");
-      double? catetoB = double.tryParse(stdin.readLineSync()!);
-
-      hipotenusaTriangulo(catetoA, catetoB);
-      break;
-    case 6:
-      double? temperaturaCelsius = 70.0;
-      double temperaturaFahrenheit = celsiusToFahrenheit(temperaturaCelsius);
-      print("${temperaturaCelsius.toStringAsFixed(2)}°C es equivalente a ${temperaturaFahrenheit.toStringAsFixed(2)}°F");
-      break;
-    default:
-      print("Opción no válida.");
+        hipotenusaTriangulo(catetoA, catetoB);
+        continue;
+      case 6:
+        print("Ingrese la temperatura en grados Celsius:");
+        double? temperaturaCelsius = double.tryParse(stdin.readLineSync()!);
+        double temperaturaFahrenheit = celsiusToFahrenheit(temperaturaCelsius);
+        print(
+          "${temperaturaCelsius}°C es equivalente a ${temperaturaFahrenheit.toStringAsFixed(2)}°F",
+        );
+        continue;
+      case 7:
+        print("Saliendo del programa...");
+        exit(0);
+      default:
+        print("Opción no válida.");
+    }
   }
 }
 
